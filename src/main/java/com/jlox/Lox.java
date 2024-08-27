687f41c5f357896f311c48a93e5d1447092fab08
ChosunOne <chosunone@protonmail.com>
2024-08-27 23:54:33 +0900

    feat: turing complete

--- src/main/java/com/jlox/Lox.java
+++ src/main/java/com/jlox/Lox.java
@@ -9,7 +9,9 @@ import java.io.InputStreamReader;
 import java.util.List;
 
 public class Lox {
+        private static final Interpreter interpreter = new Interpreter();
         static boolean hadError = false;
+        static boolean hadRuntimeError = false;
 
         public static void main(String[] args) throws IOException {
                 if (args.length > 1) {
@@ -27,6 +29,8 @@ public class Lox {
                 run(new String(bytes, Charset.defaultCharset()));
                 if (hadError)
                         System.exit(65);
+                if (hadRuntimeError)
+                        System.exit(70);
         }
 
         private static void runPrompt() throws IOException {
@@ -45,20 +49,43 @@ public class Lox {
         private static void run(String source) {
                 Scanner scanner = new Scanner(source);
                 List<Token> tokens = scanner.scanTokens();
+                Parser parser = new Parser(tokens);
+                List<Stmt> statements = parser.parse();
 
-                // For now, just print the tokens
-                for (Token token : tokens) {
-                        System.out.println(token);
-                }
-        }
+                // Stop if there was a syntax error
+                if (hadError)
+                        return;
 
-        static void error(int line, String message) {
-                report(line, "", message);
+                Resolver resolver = new Resolver(interpreter);
+                resolver.resolve(statements);
+
+                // Stop if there was a resolution error
+                if (hadError)
+                        return;
+
+                interpreter.interpret(statements);
         }
 
         private static void report(int line, String where, String message) {
                 System.err.println(
                                 "[line " + line + "] Error" + where + ": " + message);
                 hadError = true;
+        }
+
+        static void error(int line, String message) {
+                report(line, "", message);
+        }
+
+        static void error(Token token, String message) {
+                if (token.type == TokenType.EOF) {
+                        report(token.line, " at end", message);
+                } else {
+                        report(token.line, " at '" + token.lexeme + "'", message);
+                }
+        }
+
+        static void runtimeError(RuntimeError error) {
+                System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+                hadRuntimeError = true;
         }
 }
